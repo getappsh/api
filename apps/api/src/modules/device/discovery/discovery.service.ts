3c1eeb76b0045119ee19d71fa255eeefa56a474b
mataniss <matanissler@gmail.com>
2026-01-21 15:46:11 +0200

    fix a bug with sending restricions

--- apps/api/src/modules/device/discovery/discovery.service.ts
+++ apps/api/src/modules/device/discovery/discovery.service.ts
@@ -39,7 +39,8 @@ export class DiscoveryService {
       res.offer = []
     }
     
-
+    // Add restrictions from discovery response
+    res.restrictions = discoveryResponse.restrictions;
     
     return res
   }
