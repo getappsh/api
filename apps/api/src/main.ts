b4fca752e66196507b9a9dec0e4eae4e659f8e24
David Klein <davidk@ravtech.co.il>
2026-01-22 12:34:32 +0200

    refactor: update Swagger documentation to include Login module and add v2 endpoints

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -71,7 +71,7 @@ async function setupSwagger(app: INestApplication) {
     .addBearerAuth()
     .build();
   const fullDocumentV2 = SwaggerModule.createDocument(app, configV2, {
-    include: [DeviceModule],
+    include: [DeviceModule, Login],
   });
   const documentV2 = filterByVersion(fullDocumentV2, '2');
   SwaggerModule.setup('docs/v2', app, documentV2, { swaggerOptions: { docExpansion: 'none' } });
@@ -84,7 +84,7 @@ async function setupSwagger(app: INestApplication) {
     .addBearerAuth()
     .build();
   const fullDocumentV2Device = SwaggerModule.createDocument(app, configV2Device, {
-    include: [DeviceModule],
+    include: [DeviceModule, Login],
   });
   const documentV2Device = filterByVersion(fullDocumentV2Device, '2');
   SwaggerModule.setup('docs/v2/device', app, documentV2Device, { swaggerOptions: { docExpansion: 'none' } });
